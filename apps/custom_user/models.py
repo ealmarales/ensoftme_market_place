2dcf579c44b3368e2a9e65881fc648fc5cbb7be7
Erisel Almarales <erisel.almarales@gmail.com>
2023-05-31 17:55:54 -0400

    adding beneficiaries to user profile

--- apps/custom_user/models.py
+++ apps/custom_user/models.py
@@ -6,6 +6,16 @@ from django_google_maps import fields as map_fields
 User = get_user_model()
 
 
+class Beneficiary(models.Model):
+    beneficiary = models.ForeignKey(User, on_delete=models.CASCADE)
+    decription = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        if self.decription:
+            return self.decription
+        return self.beneficiary
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -14,6 +24,8 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=10, null=True, blank=True)
     address = map_fields.AddressField(max_length=200, null=True, blank=True)
     geolocation = map_fields.GeoLocationField(max_length=100, null=True, blank=True)
+
+    beneficiaries = models.ManyToManyField(Beneficiary, related_name='beneficiaries')
 
     def __str__(self):
         return self.user.__str__()
