d40d587ab1703f0ec13d799de7dc1b2bd0a80715
Erisel Almarales <erisel.almarales@gmail.com>
2023-05-31 10:50:16 -0400

    user profile maketation

--- apps/custom_user/views.py
+++ apps/custom_user/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
 
 
 class ProfilePageView(TemplateView):
-    template_name = "market/profile.html"
+    template_name = "market/profile/profile.html"
+
+
+class ProfileContactPageView(TemplateView):
+    template_name = "market/profile/profile_contacts.html"
+
+
+class ProfileAddressPageView(TemplateView):
+    template_name = "market/profile/profile_address.html"
