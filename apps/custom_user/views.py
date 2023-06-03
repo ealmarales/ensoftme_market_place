5965ceecef4f1aab310f9a27348fac8245d52537
Teudis Naranjo <teudis2008@gmail.com>
2023-06-02 21:31:37 -0400

    Added icons

--- apps/custom_user/views.py
+++ apps/custom_user/views.py
@@ -1,16 +1,16 @@
-# from django.shortcuts import render
+from django. contrib. auth. mixins import LoginRequiredMixin
 
 # Create your views here.
 from django.views.generic import TemplateView
 
 
-class ProfilePageView(TemplateView):
+class ProfilePageView(LoginRequiredMixin, TemplateView):
     template_name = "market/profile/profile.html"
 
 
-class ProfileContactPageView(TemplateView):
+class ProfileContactPageView(LoginRequiredMixin, TemplateView):
     template_name = "market/profile/profile_contacts.html"
 
 
-class ProfileAddressPageView(TemplateView):
+class ProfileAddressPageView(LoginRequiredMixin, TemplateView):
     template_name = "market/profile/profile_address.html"
